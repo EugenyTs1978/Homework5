47f355b7a18f68f12ad60cc15307ecd472d2df84
Eugeny1978 <eugenytsypyshev@rambler.ru>
2022-09-30 16:11:35 +0500

    Домашнее задание5

---
+++ Задача34/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+ Напишите программу, которая покажет количество чётных чисел в массиве.
+ */
+ int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+int CountOfEvenNumbers(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i < array.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+Console.WriteLine("Введите длину массива");
+int userLength = Convert.ToInt32(Console.ReadLine());
+int[] userArray = CreateArray(userLength);
+int isCount = CountOfEvenNumbers(userArray);
+PrintArray(userArray);
+Console.Write($"Количество четных чисел в массиве равно {isCount}");
